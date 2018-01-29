e98a418ee56884ff2c1eae1cf6de99856ecead4a
Nick Collins <thespaniard514@gmail.com>
2018-01-29 16:05:56 -0700

    final

--- AutoChickenDoor/AutoChickenDoor.ino
+++ AutoChickenDoor/AutoChickenDoor.ino
@@ -1,51 +1,64 @@
 const int lightSensorPin = A0; // light sensor analog
-// LED pins (green for closed/safe red for open/danger)
-const int redLedPin = 2;
-const int greenLedPin = 3;
+
 // I am using 2 relays to control the motor direction instead of a proper motor controller (easier to get my hands on)
 // therefore I am using  the two pins below
 const int counterClockwiseRelayPin = 4; 
 const int clockwiseRelayPin = 5;
 // the reed switches sit at the top and bottom of the door with a magnet in the door to trigger the sensors
-const int bottomReedPin = 11;
-const int topReedPin = 12;
-const int waitTimeLightSensor = 30000;
+const int bottomReedPin = 7;
+const int topReedPin = 8;
+const int waitTimeLightSensor = 20000;
 
-bool doSerial = false;
+bool doSerial = true;
 int analogValue = 0; // init the analog value to zero
+
+void motorOff(){
+  digitalWrite(counterClockwiseRelayPin, HIGH);  
+  digitalWrite(clockwiseRelayPin, HIGH);
+}
 
 void close_door(){
   // loop until the reed sensor shows HIGH
   while(digitalRead(bottomReedPin) == LOW){
-    if(digitalRead(counterClockwiseRelayPin) == LOW){
-      if(doSerial){Serial.println("Closing Door");}
-      digitalWrite(counterClockwiseRelayPin, HIGH);  
-    }    
+    if(digitalRead(counterClockwiseRelayPin) == HIGH){
+      if(doSerial){Serial.println("Closing Door");}      
+      if(digitalRead(clockwiseRelayPin) == LOW){
+        digitalWrite(clockwiseRelayPin, HIGH);
+      }
+      if(digitalRead(counterClockwiseRelayPin) == HIGH){
+        digitalWrite(counterClockwiseRelayPin, LOW);
+      }
+    }
     delay(200);
   }
   delay(400);
-  digitalWrite(counterClockwiseRelayPin, LOW);  
+  motorOff();
   if(doSerial){Serial.println("Door is Closed");}
 }
 
 void open_door(){
   // loop until the reed sensor shows HIGH
   while(digitalRead(topReedPin) == LOW){
-    if(digitalRead(clockwiseRelayPin) == LOW){
+    if(digitalRead(clockwiseRelayPin) == HIGH){
       if(doSerial){Serial.println("Opening Door");}
-      digitalWrite(clockwiseRelayPin, HIGH);
+      if(digitalRead(counterClockwiseRelayPin) == LOW){
+        digitalWrite(counterClockwiseRelayPin, HIGH);
+      }
+      if(digitalRead(clockwiseRelayPin) == HIGH){
+        digitalWrite(clockwiseRelayPin, LOW);
+      }
     }
-    delay(100);
+    delay(200);
   }
-  digitalWrite(clockwiseRelayPin, LOW);
+  motorOff();
   if(doSerial){Serial.println("Door is Open");}
 }
 
 void setup() {
-  pinMode(redLedPin, OUTPUT);
-  pinMode(greenLedPin, OUTPUT);
   pinMode(clockwiseRelayPin, OUTPUT);
+  digitalWrite(clockwiseRelayPin, HIGH);
   pinMode(counterClockwiseRelayPin, OUTPUT);
+  digitalWrite(counterClockwiseRelayPin, HIGH);
   pinMode(bottomReedPin, INPUT);
   pinMode(topReedPin, INPUT);
   if(doSerial){
@@ -64,23 +77,11 @@ void loop() {
     Serial.println(digitalRead(topReedPin));
   }
   
-  if(analogValue < 2){
-    if(digitalRead(greenLedPin) == LOW){
-      digitalWrite(greenLedPin, HIGH);
-    }
-    if(digitalRead(redLedPin) == HIGH){
-      digitalWrite(redLedPin, LOW);
-    }
+  if(analogValue < 7){
     close_door();
-  }else if(analogValue > 20){
-    if(digitalRead(greenLedPin) == HIGH){
-      digitalWrite(greenLedPin, LOW);
-    }
-    if(digitalRead(redLedPin) == LOW){
-      digitalWrite(redLedPin, HIGH);
-    }
+  }else if(analogValue > 18){
     open_door();
-    }
+  }
   delay(waitTimeLightSensor);
 }
 
