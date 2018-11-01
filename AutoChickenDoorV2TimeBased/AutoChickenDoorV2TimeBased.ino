baf52f1ba64906ad43bac7bc6ea206c4832de460
Nick Collins <thespaniard514@gmail.com>
2018-11-01 09:47:21 -0600

    time based and sensor based so we don't over work the motor if sensors don't trigger

--- AutoChickenDoorV2TimeBased/AutoChickenDoorV2TimeBased.ino
+++ AutoChickenDoorV2TimeBased/AutoChickenDoorV2TimeBased.ino
@@ -7,7 +7,7 @@ const int topReedPin = 8;
 
 const int downThreshold = 1;
 const int upThreshold = 15;
-const int waitTimeLightSensor = 13000;
+const int waitTimeLightSensor = 15000;
 const int waitTimeManualMode = 3000;
 
 bool doSerial = true;
@@ -20,8 +20,8 @@ int toggleState = LOW;
 unsigned long starttime = 0;
 unsigned long endtime = 0;
 
-int closeTime = 7500;
-int openTime = 8000;
+int closeTime = 9000;
+int openTime = 11000;
 
 bool isOpen = true;
 
@@ -74,6 +74,9 @@ void setup() {
   if(doSerial){
     Serial.begin(9600);  
   }  
+  if(digitalRead(bottomReedPin) == HIGH){
+    isOpen = false;
+  }
 }
 
 void loop() {
@@ -81,6 +84,10 @@ void loop() {
     if(doSerial){
       Serial.print("Light Sensor Reading ");
       Serial.println(analogValue);
+      Serial.print("Bottom Reed: ");
+      Serial.println(digitalRead(bottomReedPin));
+      Serial.print("Top Reed: ");
+      Serial.println(digitalRead(topReedPin));
     }
     
     if(analogValue < downThreshold && isOpen){
